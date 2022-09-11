9904ad4a29d09cac98f4ede53080a58d32671ea9
Melimet <erkka.rahikainen@gmail.com>
2022-09-11 21:45:18 +0300

    Exercises 4,5,6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,18 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        found_ratings = Rating.where beer_id: self.id
+        if found_ratings == [] or found_ratings == nil
+            return  "This beer has no ratings yet!"
+        end
+        average = found_ratings.reduce(0){|sum,x| sum + x.score }.to_f / found_ratings.length.to_f
+        
+        
+        "This beer has "+ found_ratings.length.to_s + " rating".pluralize(found_ratings.length) +" with a rating of "+ average.to_s
+        
+    end
+
+    
 end
