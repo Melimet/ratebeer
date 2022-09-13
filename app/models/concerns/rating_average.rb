dae0007971945dc1a03b022e653b7fd39a727ae4
Melimet <erkka.rahikainen@gmail.com>
2022-09-13 16:58:51 +0300

    Clean up spaghetti

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -1,13 +1,7 @@
 module RatingAverage
     extend ActiveSupport::Concern
 
-    def average_rating
-        #found_ratings = "paska, #{RatingAverage.class}"
-        return 1
-        return found_ratings
-        if found_ratings == [] or found_ratings == nil
-            return nil
-        end
-        average = found_ratings.reduce(0){|sum,x| sum + x.score }.to_f / found_ratings.length.to_f
-    end
+  def average_rating
+    ratings.average(:score).to_f
+  end
 end
