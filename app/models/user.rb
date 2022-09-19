7c749cd99f5de69249343e0f4d37f2e88b55be77
Melimet <erkka.rahikainen@gmail.com>
2022-09-19 21:11:50 +0300

    Fix relations between user and beer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   include RatingAverage
+
   has_secure_password
-  has_many :ratings
+
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
+  
   has_many :memberships
   has_many :beer_clubs, through: :memberships
 
@@ -12,11 +15,13 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 4 },
     format: { with: /\A[A-Z].*\d|\d.*[A-Z]\z/, message: "Password must contain atleast 1 upper case letter and a numeral" }
 
-  has_many :ratings
-
   def favorite_beer
     return nil if ratings.empty?
     ratings.sort_by(&:score).last.beer
+  end
+
+  def favorite_style
+    ##Tee hashmap ja +1 aina ku tulee osuma
   end
 
   def to_s
