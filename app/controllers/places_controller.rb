af13ea6b650eda2c41042a0b7a4ba77b0a003798
Melimet <erkka.rahikainen@gmail.com>
2022-09-22 20:45:19 +0300

    Add view for single bar

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,12 +2,31 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def show
+    places = Rails.cache.fetch(session[:last_search])
+
+    places.each do |place|
+        if place.send("id") == params[:id]
+            @place = place
+            return
+        end
+    end
+    redirect_to places_path, notice: "No such bar!"
+  end
+
   def search
-    @places = BeermappingApi.places_in(params[:city])
+    city = params[:city].downcase
+    @places = BeermappingApi.places_in(city)
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
+      session[:last_search] = ERB::Util.url_encode(city)
       render :index, status: 418
     end
+  end
+
+  def to_s
+
+    @place
   end
 end
--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,6 +1,8 @@
 class BeermappingApi
+
+
   def self.places_in(city)
-    city = city.downcase
+    
     Rails.cache.fetch(city, expires_in: 1.week) { get_places_in(city) }
   end
 
@@ -9,12 +11,13 @@ class BeermappingApi
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
-
+    
     return [] if places.is_a?(Hash) && places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
     places.map do |place|
       Place.new(place)
+      
     end
   end
 
