102350bb9d5e92342d605785131b8b6f70e4773b
Melimet <erkka.rahikainen@gmail.com>
2022-09-23 22:00:20 +0300

    Make navbar cuter

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -2,7 +2,7 @@ class BeerClub < ApplicationRecord
   has_many :memberships
   has_many :users, through: :memberships
 
-  def a_member?(user)
+  def member_in?(user)
     users.each do |i|
       return true if i.username == user.username
     end
